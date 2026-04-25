565f2861da6048055d201c76b27337636412ea09
Mohanad Abusabha <eng.mohanadsabha@gmail.com>
2026-04-25 16:00:37 +0300

    Setup OAuth

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -7,6 +7,10 @@ const router = Router();
 // Required auth routes
 router.post("/signup", authController.signUp);
 router.post("/login", authController.login);
+router.get("/linkedin/callback", authController.linkedinCallback);
+router.get("/linkedin", authController.linkedin);
+router.get("/google/callback", authController.googleCallback);
+router.get("/google", authController.google);
 router.post("/verify-email", authController.verifyEmail);
 router.post("/refresh-token", authController.refreshToken);
 router.post("/validate-token", authController.validateToken);
